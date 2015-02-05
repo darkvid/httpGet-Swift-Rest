e7ee80f78b6c86ef0fea658482d1f4249518687e
darkvid <David Galisteo>
2015-02-05 14:53:25 +0100

    httpGet Swift Rest

---
+++ section-1.swift
@@ -0,0 +1,37 @@
+// Playground - noun: a place where people can play
+
+import Foundation
+import XCPlayground
+import UIKit
+
+
+func httpGet(url: String, callback: (String, String?) -> Void){
+
+    var request = NSMutableURLRequest(URL: NSURL(string: url)!)
+    var session = NSURLSession.sharedSession()
+    var task = session.dataTaskWithRequest(request){
+        (data, response, error) -> Void in
+        if error != nil {
+            callback("", error.localizedDescription)
+        }else{
+            var result = NSString(data: data, encoding: NSASCIIStringEncoding)!
+            callback(result, nil)
+        }
+    }
+    task.resume()
+}
+
+
+func callbackGet (a: String, b: String?) -> Void{
+    println(a)
+}
+
+
+
+httpGet("http://localhost/tfg_server/1/eventos", callbackGet)
+
+
+
+
+//XCPSetExecutionShouldContinueIndefinitely(continueIndefinitely: true)
+
